d47d81998a25978dc385f0373b0f50c92cc13610
Jordan Hall <jordan@libertyware.co.uk>
2023-07-25 18:58:23 +0100

    fix: deploy this app

--- src/caprover.ts
+++ src/caprover.ts
@@ -1,20 +1,50 @@
 import * as core from '@actions/core';
+import fetch from 'node-fetch'
+
+interface CaproverApps {
+  appName: string
+  volumes: { volumeName: string }[]
+}
 
 export class CapRover {
   private url: string;
-  private password: string;
+  private tokenPromise: Promise<string>;
 
   constructor(url: string, password: string) {
     this.url = url;
-    this.password = password;
+    this.tokenPromise = this.login(password);
+  }
+
+  private async login(password: string): Promise<string> {
+    try {
+      const response = await fetch(`http://${this.url}/api/v2/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({"password": password}),
+      });
+      const data = await response.json() as { token: string };
+      core.setOutput('response', data);
+      return data.token;
+    } catch (error) {
+      core.setFailed(`Failed to create application: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async getTokenOrError() {
+    const timeout = new Promise<string>((_, reject) =>
+      setTimeout(() => reject(new Error('Timeout waiting for token')), 2 * 60 * 1000) // 2 minutes
+    );
+    return await Promise.race([this.tokenPromise, timeout]);
   }
 
   async createApp(appName: string) {
     try {
-      const response = await fetch(`http://${this.url}/app/register/${appName}`, {
+      const token = await this.getTokenOrError();
+      const response = await fetch(`http://${this.url}/api/v2/user/apps/appDefinitions/register`, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ password: this.password }),
+        headers: { 'Content-Type': 'application/json', 'x-captain-auth': token },
+        body: JSON.stringify({"appName": appName, "hasPersistentData": true}),
       });
       const data = await response.json();
       core.setOutput('response', data);
@@ -23,15 +53,16 @@ export class CapRover {
     }
   }
 
-  async deployApp(appName: string, imageTag: string) {
+  async deployApp(appName: string, imageTag: string, imageName?: string) {
     try {
-      const response = await fetch(`http://${this.url}/app/deploy/${appName}`, {
+      const token = await this.getTokenOrError();
+      const response = await fetch(`http://${this.url}/api/v2/user/apps/appData/${appName}`, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json', 'x-captain-auth': token },
         body: JSON.stringify({
-          password: this.password,
-          imageTag: imageTag,
-        }),
+          "schemaVersion": 2,
+          "imageName": `${imageName || appName}:${imageTag}`
+      }),
       });
       const data = await response.json();
       core.setOutput('response', data);
@@ -40,12 +71,47 @@ export class CapRover {
     }
   }
 
+  async getApp(appName: string) {
+    try {
+      const list = await this.getList();
+      const app = list.appDefinitions.find(app => app.appName === appName);
+      if (!app) {
+        throw new Error(`App ${appName} not found.`);
+      }
+      return app;
+    } catch (error) {
+      core.setFailed(`Failed to fetch app: ${error.message}`);
+    }
+    return null;
+  }
+
+  async getList(): Promise<{ appDefinitions: CaproverApps[] }> {
+    try {
+      const token = await this.getTokenOrError();
+      const response = await fetch(`http://${this.url}/api/v2/user/apps/appDefinitions`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', 'x-captain-auth': token },
+      });
+      const data = await response.json() as { appDefinitions: CaproverApps[] };
+      core.setOutput('response', data);
+      return data;
+    } catch (error) {
+      core.setFailed(`Failed to fetch list: ${error.message}`);
+    }
+    return { appDefinitions: [] }
+  }
+
   async deleteApp(appName: string) {
     try {
-      const response = await fetch(`http://${this.url}/app/delete/${appName}`, {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ password: this.password }),
+      const token = await this.getTokenOrError();
+      const app = await this.getApp(appName)
+      const response = await fetch(`http://${this.url}/api/v2/user/apps/appDefinitions/delete`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'x-captain-auth': token },
+        body: JSON.stringify({
+          appName: appName,
+          volumes: app?.volumes?.map(v => v.volumeName)
+        })
       });
       const data = await response.json();
       core.setOutput('response', data);
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ async function run() {
     const capRoverUrl = core.getInput('caprover_url', { required: true });
     const password = core.getInput('caprover_password', { required: true });
     const appName = core.getInput('app_name', { required: true });
-    const imageTag = core.getInput('image_tag');
+    const imageName = core.getInput('image_name', { required: false });
+    const imageTag = core.getInput('image_tag', { required: true });
     const operation = core.getInput('operation', { required: true });
 
     const caprover = new CapRover(capRoverUrl, password);
@@ -16,7 +17,7 @@ async function run() {
         await caprover.createApp(appName);
         break;
       case 'deploy':
-        await caprover.deployApp(appName, imageTag);
+        await caprover.deployApp(appName, imageTag, imageName);
         break;
       case 'cleanup':
         await caprover.deleteApp(appName);
